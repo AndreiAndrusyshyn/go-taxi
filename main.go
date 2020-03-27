12d4bc605caf7631c8d4bd6497a0e673b5ae2369
hyper_drus <andrusishin.av@gmail.com>
2020-03-27 03:12:52 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-redis/redis/v7"
+	"math/rand"
+	"time"
+)
+
+
+
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	client.FlushAll()
+	rand.Seed(time.Now().Unix())
+
+	var letters = [26]string{"a", "b", "c", "d" ,"e", "f", "g", "h", "i", "j", "k", "l",
+		"m","n","o", "p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"}
+
+	for i := 0; i < 50; i++ {
+		var two_random_letters = letters[rand.Intn(len(letters))] + letters[rand.Intn(len(letters))]
+		var res , _ = client.HSetNX("order", two_random_letters, 0).Result()
+		if !res { i-- }
+	}
+
+	for {
+		val2, err := client.HKeys("order").Result()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("key2222",val2[rand.Intn(len(val2))])
+
+		client.HDel("order", val2[rand.Intn(len(val2))])
+
+		for {
+			var two_random_letters = letters[rand.Intn(len(letters))] + letters[rand.Intn(len(letters))]
+			var res , _ = client.HSetNX("order", two_random_letters, 0).Result()
+			if res { break }
+		}
+
+
+		val3, err := client.HKeys("order").Result()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("key",len(val3))
+
+		time.Sleep(200 * time.Millisecond)
+	}
+
+
+	//ExampleClient()
+}
+
+
